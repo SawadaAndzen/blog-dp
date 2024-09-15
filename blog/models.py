2f4e90cc00455ccc8fa4407921fa8263a7449c52
Sawada <105200073+SawadaAndzen@users.noreply.github.com>
2024-09-15 14:39:26 +0300

    finish2

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,24 @@
 from django.db import models
 
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    bio = models.TextField()
+    
+    def __str__(self):
+        
+        return self.name
+    
+    
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField()
+    author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
     
     def __str__(self):
         
         return self.title
+    
+    def published_recently():
+        ...
--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.read_posts, name='post_list'),
+    path('', views.read_home, name='home'),
+    path('posts/', views.read_posts, name='post_list'),
+    path('authors/', views.read_authors, name='author_list'),
+    path('post/<int:post_id>', views.read_post, name='post'),
+    path('author/<int:author_id>', views.read_author, name='author'),
 ]
